c7c04ccf2fb9ecfb3c5b693fd41ef44775f114be
metairie <stephane.metairie@email.com>
2022-01-03 19:13:58 +0100

    add compose

---
+++ container/install_compose.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o docker-compose
+chmod +x docker-compose
+mv docker-compose /usr/bin/
