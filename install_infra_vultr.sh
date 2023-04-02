3578bbc8c4e4735c6ff0fdbd074f83329215cb6d
LAPTOP-V810OJ39\steph <stephane.metairie@gmail.com>
2023-04-02 15:44:25 +0200

    remove centos

--- install_infra_vultr.sh
+++ install_infra_vultr.sh
@@ -139,47 +139,29 @@ for t in ${NODES_COUNT[@]}; do
   echo "NODE_INTERNAL_IP = $NODE_INTERNAL_IP"
   echo "NODE_MAIN_IP     = $NODE_MAIN_IP"
   echo "-----------------------------"
+  echo "    ❤️ Ubuntu Linux detected"
+  ssh -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP" "ip a | grep -iA2 '3: enp' | grep -i 'link/ether' | cut -d' ' -f6 > $file_MACADDRESS"
+  ssh -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP" "ip a | grep -i '3: enp' | cut -d':' -f2 | tr -d ' ' > $file_NETINTERFACE"
+  scp -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP:$file_MACADDRESS $file_MACADDRESS"
+  scp -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP:$file_NETINTERFACE $file_NETINTERFACE"
+  MACADDRESS=`cat $file_MACADDRESS`
+  NETINTERFACE=`cat $file_NETINTERFACE`
+  echo $MACADDRESS
+  echo $NETINTERFACE
+  cat $file_MACADDRESS
+  cat $file_NETINTERFACE
+  localfile="/tmp/10-$NETINTERFACE.txt"
+  netfile="10-$NETINTERFACE"
+  echo $localfile
+  echo $netfile
+  cp -f net-ubuntu.tmpl "$localfile"
+  echo "  🖧 - NODE ${NODE_LABEL} ip=${NODE_MAIN_IP} setup private interface ${NODE_INTERNAL_IP}"
+  sed -i 's/#IPV4#/'${NODE_INTERNAL_IP}'/g' "$localfile"
+  sed -i 's/#NETINTERFACE#/'$NETINTERFACE'/g' "$localfile"
+  sed -i 's/#MACADDRESS#/'$MACADDRESS'/g' "$localfile"
+  scp -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" "$localfile" root@"$NODE_MAIN_IP:/etc/netplan/$netfile"
+  ssh -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP" "netplan apply"
 
-  if [[ $osid == "$CENTOS" ]]; then
-    echo "    CentOS Linux detected"
-    ssh -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP" "nmcli | grep 'disconnected' | cut -d':' -f1 > $file_NETINTERFACE"
-    scp -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP":$file_NETINTERFACE $file_NETINTERFACE
-    NETINTERFACE=`cat $file_NETINTERFACE`
-    localfile="/tmp/ifcfg-$NETINTERFACE.yaml"
-    netfile="ifcfg-$NETINTERFACE"
-    echo "Capture itf name : $localfile"
-    cp -f net-centos8.tmpl "$localfile"
-    echo "  🖧 - NODE ${NODE_LABEL} ip=${NODE_MAIN_IP} setup private interface ${NODE_INTERNAL_IP}"
-    sed -i 's/#IPV4#/'${NODE_INTERNAL_IP}'/g' "$localfile"
-    sed -i 's/#NETINTERFACE#/'$NETINTERFACE'/g' "$localfile"
-    scp -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" "$localfile" root@"$NODE_MAIN_IP:/etc/sysconfig/network-scripts/$netfile"
-    ssh -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP" "nmcli con load /etc/sysconfig/network-scripts/$netfile"
-    ssh -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP" "nmcli con up 'System "$NETINTERFACE"'"
-    fi
-  if [[ $osid == "$UBUNTU" ]]; then
-    echo "    ❤️ Ubuntu Linux detected"
-    ssh -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP" "ip a | grep -iA2 '3: enp' | grep -i 'link/ether' | cut -d' ' -f6 > $file_MACADDRESS"
-    ssh -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP" "ip a | grep -i '3: enp' | cut -d':' -f2 | tr -d ' ' > $file_NETINTERFACE"
-    scp -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP:$file_MACADDRESS $file_MACADDRESS"
-    scp -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP:$file_NETINTERFACE $file_NETINTERFACE"
-    MACADDRESS=`cat $file_MACADDRESS`
-    NETINTERFACE=`cat $file_NETINTERFACE`
-    echo $MACADDRESS
-    echo $NETINTERFACE
-    cat $file_MACADDRESS
-    cat $file_NETINTERFACE
-    localfile="/tmp/10-$NETINTERFACE.txt"
-    netfile="10-$NETINTERFACE"
-    echo $localfile
-    echo $netfile
-    cp -f net-ubuntu.tmpl "$localfile"
-    echo "  🖧 - NODE ${NODE_LABEL} ip=${NODE_MAIN_IP} setup private interface ${NODE_INTERNAL_IP}"
-    sed -i 's/#IPV4#/'${NODE_INTERNAL_IP}'/g' "$localfile"
-    sed -i 's/#NETINTERFACE#/'$NETINTERFACE'/g' "$localfile"
-    sed -i 's/#MACADDRESS#/'$MACADDRESS'/g' "$localfile"
-    scp -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" "$localfile" root@"$NODE_MAIN_IP:/etc/netplan/$netfile"
-    ssh -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP" "netplan apply"
-  fi
   if [[ -f "$localfile" ]]; then
     rm "$localfile"
   fi
