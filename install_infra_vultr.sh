718139c363b95da561b21768ab373692d068054d
LAPTOP-V810OJ39\steph <stephane.metairie@gmail.com>
2023-04-02 16:40:01 +0200

    restore full

--- install_infra_vultr.sh
+++ install_infra_vultr.sh
@@ -77,77 +77,77 @@ echo " ----------------------------"
 echo "Get SSH key for accessing servers"
 SSHKEY_ID=`curl -s "https://api.vultr.com/v2/ssh-keys"   -X GET   -H "Authorization: Bearer ${VULTR_API_KEY}" | jq '.ssh_keys[].id' | tr -d '"'`
 
-# echo " ---------------------------------"
-# echo "🐇 Fast creation of masters and nodes 🐇"
-# echo " ---------------------------------"
-# for node in $nodelist
-# do
-#   if [[ ${node} =~ "CONSOLE" ]]; then
-#     plan=$plan_console
-#     ((number_node++))
-#   fi
-#   if [[ ${node} =~ "MASTER" ]]; then
-#     plan=$plan_master
-#     ((number_node++))
-#   fi
-#   if [[ ${node} =~ "NODE" ]]; then 
-#     plan=$plan_node
-#     ((number_node++))
-#   fi
-# DATA='{"region":"'$region'",
-# "plan":"'$plan'",
-# "label":"'$node'",
-# "hostname":"'$node'",
-# "os_id":'$osid',
-# "attach_private_network":["'$APN'"],
-# "sshkey_id":["'$SSHKEY_ID'"]
-# }'
-#   echo "Create node: $node"
-#   curl -s "https://api.vultr.com/v2/instances" -X POST -H "Authorization: Bearer ${VULTR_API_KEY}" -H "Content-Type: application/json" --data "${DATA}"
-#   echo
-# done
+echo " ---------------------------------"
+echo "🐇 Fast creation of masters and nodes 🐇"
+echo " ---------------------------------"
+for node in $nodelist
+do
+  if [[ ${node} =~ "CONSOLE" ]]; then
+    plan=$plan_console
+    ((number_node++))
+  fi
+  if [[ ${node} =~ "MASTER" ]]; then
+    plan=$plan_master
+    ((number_node++))
+  fi
+  if [[ ${node} =~ "NODE" ]]; then 
+    plan=$plan_node
+    ((number_node++))
+  fi
+DATA='{"region":"'$region'",
+"plan":"'$plan'",
+"label":"'$node'",
+"hostname":"'$node'",
+"os_id":'$osid',
+"attach_private_network":["'$APN'"],
+"sshkey_id":["'$SSHKEY_ID'"]
+}'
+  echo "Create node: $node"
+  curl -s "https://api.vultr.com/v2/instances" -X POST -H "Authorization: Bearer ${VULTR_API_KEY}" -H "Content-Type: application/json" --data "${DATA}"
+  echo
+done
 
-# nseconds=$((30+number_node*20))
-# echo " ---------------------------------"
-# echo " ⌛⌛⌛ Wait provisionning finishes ... $nseconds seconds ⌛⌛⌛"
-# echo " ---------------------------------"
-# sleep $nseconds
-# echo
+nseconds=$((30+number_node*20))
+echo " ---------------------------------"
+echo " ⌛⌛⌛ Wait provisionning finishes ... $nseconds seconds ⌛⌛⌛"
+echo " ---------------------------------"
+sleep $nseconds
+echo
 
-# echo " ---------------------------------"
-# echo "👺 Get Nodes and 🤖 set internal interface "
-# echo " ---------------------------------"
+echo " ---------------------------------"
+echo "👺 Get Nodes and 🤖 set internal interface "
+echo " ---------------------------------"
 
-# NODES=`curl -s "https://api.vultr.com/v2/instances" -X GET -H "Authorization: Bearer ${VULTR_API_KEY}" | jq '.'`
-# NODES_COUNT=`echo $NODES | jq '.instances' | grep -i '"id"' | tr -d "," | cut -d ":" -f2 | tr -d " " | tr -d '"'`
-# for t in ${NODES_COUNT[@]}; do
-#   NODE=`curl -s "https://api.vultr.com/v2/instances/${t}" -X GET -H "Authorization: Bearer ${VULTR_API_KEY}" | jq '.'`
-#   NODE_LABEL=`echo $NODE | jq '.instance.label' | tr -d '"'`
-#   NODE_INTERNAL_IP=`echo $NODE | jq '.instance.internal_ip' | tr -d '"'`
-#   NODE_MAIN_IP=`echo $NODE | jq '.instance.main_ip' | tr -d '"'`
-#   if [[ ${node} =~ "MASTER" || ${node} =~ "NODE" ]]; then
-#     echo "    ❤️ Ubuntu Linux detected ${NODE_MAIN_IP} / ${NODE_INTERNAL_IP}"
-#     ssh -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP" "ip a | grep -iA2 '3: enp' | grep -i 'link/ether' | cut -d' ' -f6 > $file_MACADDRESS"
-#     ssh -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP" "ip a | grep -i '3: enp' | cut -d':' -f2 | tr -d ' ' > $file_NETINTERFACE"
-#     scp -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP:$file_MACADDRESS $file_MACADDRESS"
-#     scp -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP:$file_NETINTERFACE $file_NETINTERFACE"
-#     MACADDRESS=`cat $file_MACADDRESS`
-#     NETINTERFACE=`cat $file_NETINTERFACE`
-#     localfile="/tmp/10-$NETINTERFACE.txt"
-#     netfile="10-$NETINTERFACE"
-#     cp -f net-ubuntu.tmpl "$localfile"
-#     echo "  🖧 - NODE ${NODE_LABEL} ip=${NODE_MAIN_IP} setup private interface ${NODE_INTERNAL_IP}"
-#     sed -i 's/#IPV4#/'${NODE_INTERNAL_IP}'/g' "$localfile"
-#     sed -i 's/#NETINTERFACE#/'$NETINTERFACE'/g' "$localfile"
-#     sed -i 's/#MACADDRESS#/'$MACADDRESS'/g' "$localfile"
-#     scp -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" "$localfile" root@"$NODE_MAIN_IP:/etc/netplan/$netfile"
-#     ssh -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP" "netplan apply"
+NODES=`curl -s "https://api.vultr.com/v2/instances" -X GET -H "Authorization: Bearer ${VULTR_API_KEY}" | jq '.'`
+NODES_COUNT=`echo $NODES | jq '.instances' | grep -i '"id"' | tr -d "," | cut -d ":" -f2 | tr -d " " | tr -d '"'`
+for t in ${NODES_COUNT[@]}; do
+  NODE=`curl -s "https://api.vultr.com/v2/instances/${t}" -X GET -H "Authorization: Bearer ${VULTR_API_KEY}" | jq '.'`
+  NODE_LABEL=`echo $NODE | jq '.instance.label' | tr -d '"'`
+  NODE_INTERNAL_IP=`echo $NODE | jq '.instance.internal_ip' | tr -d '"'`
+  NODE_MAIN_IP=`echo $NODE | jq '.instance.main_ip' | tr -d '"'`
+  if [[ ${node} =~ "MASTER" || ${node} =~ "NODE" ]]; then
+    echo "    ❤️ Ubuntu Linux detected ${NODE_MAIN_IP} / ${NODE_INTERNAL_IP}"
+    ssh -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP" "ip a | grep -iA2 '3: enp' | grep -i 'link/ether' | cut -d' ' -f6 > $file_MACADDRESS"
+    ssh -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP" "ip a | grep -i '3: enp' | cut -d':' -f2 | tr -d ' ' > $file_NETINTERFACE"
+    scp -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP:$file_MACADDRESS $file_MACADDRESS"
+    scp -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP:$file_NETINTERFACE $file_NETINTERFACE"
+    MACADDRESS=`cat $file_MACADDRESS`
+    NETINTERFACE=`cat $file_NETINTERFACE`
+    localfile="/tmp/10-$NETINTERFACE.txt"
+    netfile="10-$NETINTERFACE"
+    cp -f net-ubuntu.tmpl "$localfile"
+    echo "  🖧 - NODE ${NODE_LABEL} ip=${NODE_MAIN_IP} setup private interface ${NODE_INTERNAL_IP}"
+    sed -i 's/#IPV4#/'${NODE_INTERNAL_IP}'/g' "$localfile"
+    sed -i 's/#NETINTERFACE#/'$NETINTERFACE'/g' "$localfile"
+    sed -i 's/#MACADDRESS#/'$MACADDRESS'/g' "$localfile"
+    scp -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" "$localfile" root@"$NODE_MAIN_IP:/etc/netplan/$netfile"
+    ssh -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" root@"$NODE_MAIN_IP" "netplan apply"
 
-#     if [[ -f "$localfile" ]]; then
-#       rm "$localfile"
-#     fi
-#   fi
-# done
+    if [[ -f "$localfile" ]]; then
+      rm "$localfile"
+    fi
+  fi
+done
 
 
 function remove_file() {
